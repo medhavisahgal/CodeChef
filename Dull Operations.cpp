b831a940a1c867ab87c531d662deac87ee5f4a27
Medhavi Sahgal <112403556+medhavisahgal@users.noreply.github.com>
2023-10-27 15:23:00 +0530

    Create Dull Operations.cpp

---
+++ Dull Operations.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    cout<<n<<" "<<n-1<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
