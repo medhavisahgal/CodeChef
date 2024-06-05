8be4714f340e81ead78c6df69a31f3ebd7147f72
Medhavi Sahgal <112403556+medhavisahgal@users.noreply.github.com>
2024-06-05 15:14:25 +0530

    Create fever.cpp

---
+++ fever.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    if (X>98)
+	    {
+	        cout<<"YES";
+	    }
+	    else
+	    {
+	        cout<<"NO";
+	    }
+	    cout<<endl;
+	    
+	}
+	return 0;
+}
